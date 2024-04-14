cc5319e6ace4de06156d96a238176327838088d8
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2024-04-14 11:52:15 +0800

    setup development and production server

--- backend/app.js
+++ backend/app.js
@@ -5,5 +5,5 @@ import dotenv from "dotenv";
 dotenv.config({ path: "backend/config/config.env" });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on PORT: ${process.env.PORT}`);
+    console.log(`Server is running on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
     });
