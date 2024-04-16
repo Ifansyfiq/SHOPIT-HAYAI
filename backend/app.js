33e02416d7d38fbf2693b5a7fb17af59695b5441
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2024-04-16 12:03:26 +0800

    handling uncaught exception

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,20 @@ import dotenv from "dotenv";
 import { connectDatabase } from "./config/dbConnect.js";
 import errorMiddleware from "./middlewares/error.js";
 
+// handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+   console.log(`ERROR: ${err}`);
+   console.log(`Shutting down the server due to uncaught exception`);
+   process.exit(1); 
+});
+
 dotenv.config({ path: "backend/config/config.env" });
 
 // connect database
 connectDatabase();
 
 app.use(express.json());
+
 
 // import all routes
 import productRoutes from "./routes/products.js";
