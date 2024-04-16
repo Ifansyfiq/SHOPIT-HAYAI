a8b19731d40a3a583a46c138089033801497946c
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2024-04-16 16:40:28 +0800

    filters products

--- backend/utils/apiFilters.js
+++ backend/utils/apiFilters.js
@@ -16,6 +16,23 @@ class APIFilters {
         this.query = this.query.find({ ...keyword });
         return this;
     }
+
+    filters() {
+        const queryCopy = { ...this.queryStr };
+
+        //fields to exclude
+        const fieldsToRemove = ['keyword']
+        fieldsToRemove.forEach((el) => delete queryCopy[el]);
+
+
+        // Advance filter for price, ratings etc
+        
+        let queryStr = JSON.stringify(queryCopy);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);  // add $ sign before gt, gte, lt, lte
+
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
 }
 
 export default APIFilters;
