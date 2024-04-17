c91253ccd812e4cb17e6aca91e1624309f46fdaa
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2024-04-17 09:06:33 +0800

    creating User model

---
+++ backend/models/user.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter your name'],
+        maxLength: [50, 'Your name cannot exceed 50 characters'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter your email'],
+        unique: true,  // email must be unique
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter your password'],
+        minlength: [6, 'Your password must be longer than 6 characters'],
+        select: false, // hide password from user
+    },
+    avatar: {
+        public_id: {
+            type: String
+        },
+        url: {
+            type: String
+        },
+    },
+    role: {
+        type: String,
+        default: 'user', // default role is user when user register
+    },
+    resetPasswordToken: String, // token for reset password
+    resetPasswordExpire: Date, // expire time for reset password
+},
+    { timestamps: true } // show the time when user created
+);
+
+export default mongoose.model("User", userSchema); 
