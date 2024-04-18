4daadd7239ecd2ee536dbb010d26107ca7b4d055
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2024-04-18 09:33:26 +0800

    authorize user role and permissions

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -7,13 +7,15 @@ import {
     deleteProduct
 }
     from "../controllers/productControllers.js";
-import  {isAuthenticatedUser}  from "../middlewares/auth.js";
+import  {isAuthenticatedUser, authorizeRoles}  from "../middlewares/auth.js";
 const router = express.Router();
 
-router.route("/products").get(isAuthenticatedUser, getProducts);
+router.route("/products").get(isAuthenticatedUser, authorizeRoles("admin"), getProducts);
 router.route("/admin/products").post(newProducts);
+
 router.route("/products/:id").get(getProductDetails);
-router.route("/products/:id").put(updateProduct);
-router.route("/products/:id").delete(deleteProduct);
+
+router.route("/admin/products/:id").put(updateProduct);
+router.route("/admin/products/:id").delete(deleteProduct);
 
 export default router;
