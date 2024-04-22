35497840b7d1782a11a4c24c50b1243c12812288
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2024-04-22 22:33:43 +0800

    handle duplicate key & JWT error

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -6,18 +6,35 @@ export default (err, req, res, next) => {
         message: err?.message || 'Internal Server Error',
     };
 
-// handle invalid mongoose ID error
+    // handle invalid mongoose ID error
     if (err.name === 'CastError') {
         const message = `Resource not found. Invalid: ${err?.path}`
-        error =  new ErrorHandler(message, 404)
+        error = new ErrorHandler(message, 400)
     }
 
-// handle validation error
+    // handle validation error
     if (err.name === 'ValidationError') {
         const message = Object.values(err.errors).map((value) => value.message);
-        error =  new ErrorHandler(message, 404)
+        error = new ErrorHandler(message, 400)
     }
 
+    // handle mongoose duplicate key error
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+        error = new ErrorHandler(message, 400)
+    }
+
+    // handle wrong JWT error
+    if (err.name === 'JsonWebTokenError') {
+        const message = 'JSON Web Token is invalid. Try again!!!';
+        error = new ErrorHandler(message, 400)
+    }
+
+    // handle expired JWT error
+    if (err.name === 'TokenExpiredError') {
+        const message = 'JSON Web Token is expired. Try again!!!';
+        error = new ErrorHandler(message, 400)
+    }
 
     if (process.env.NODE_ENV === "DEVELOPMENT") {
         res.status(error.statusCode).json({
